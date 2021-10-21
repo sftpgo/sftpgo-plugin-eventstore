587a6acd4a3bbd6e4b6c5bf10cb2d1932ef2e5af
Nicola Murino <nicola.murino@gmail.com>
2021-10-21 22:21:34 +0200

    fix migration

--- db/migration/v1.go
+++ db/migration/v1.go
@@ -1,8 +1,6 @@
 package migration
 
 import (
-	"time"
-
 	"github.com/go-gormigrate/gormigrate/v2"
 	"gorm.io/gorm"
 )
@@ -12,15 +10,15 @@ const (
 )
 
 type providerEventV1 struct {
-	ID         string    `gorm:"primaryKey;size:36"`
-	Timestamp  time.Time `gorm:"not null;index:idx_provider_events__timestamp"`
-	Action     string    `gorm:"size:60;not null;index:idx_provider_events_action"`
-	Username   string    `gorm:"size:255;not null;index:idx_provider_events_username"`
-	IP         string    `gorm:"size:50;index:idx_provider_events_ip"`
-	ObjectType string    `gorm:"size:50;index:idx_provider_events_object_type"`
-	ObjectName string    `gorm:"size:255;index:idx_provider_events_object_name"`
+	ID         string `gorm:"primaryKey;size:36"`
+	Timestamp  int64  `gorm:"size:64;not null;index:idx_provider_events__timestamp"`
+	Action     string `gorm:"size:60;not null;index:idx_provider_events_action"`
+	Username   string `gorm:"size:255;not null;index:idx_provider_events_username"`
+	IP         string `gorm:"size:50;index:idx_provider_events_ip"`
+	ObjectType string `gorm:"size:50;index:idx_provider_events_object_type"`
+	ObjectName string `gorm:"size:255;index:idx_provider_events_object_name"`
 	ObjectData []byte
-	InstanceID string `gorm:"size:50;index:idx_provider_events_instance_id"`
+	InstanceID string `gorm:"size:60;index:idx_provider_events_instance_id"`
 }
 
 func (ev *providerEventV1) TableName() string {
@@ -28,20 +26,20 @@ func (ev *providerEventV1) TableName() string {
 }
 
 type fsEventV1 struct {
-	ID                string    `gorm:"primaryKey;size:36"`
-	Timestamp         time.Time `gorm:"not null;index:idx_fs_events_timestamp"`
-	Action            string    `gorm:"size:60;not null;index:idx_fs_events_action"`
-	Username          string    `gorm:"size:255;not null;index:idx_fs_events_username"`
-	FsPath            string    `gorm:"size:512"`
-	FsTargetPath      string    `gorm:"size:512"`
-	VirtualPath       string    `gorm:"size:512"`
-	VirtualTargetPath string    `gorm:"size:512"`
-	SSHCmd            string    `gorm:"size:60;index:idx_fs_events_ssh_cmd"`
-	FileSize          int64     `gorm:"size:64"`
-	Status            int       `gorm:"size:32;index:idx_fs_events_status"`
-	Protocol          string    `gorm:"size:30;not null;index:idx_fs_events_protocol"`
-	IP                string    `gorm:"size:50;index:idx_ip"`
-	InstanceID        string    `gorm:"size:50;index:idx_fs_events_instance_id"`
+	ID                string `gorm:"primaryKey;size:36"`
+	Timestamp         int64  `gorm:"size:64;not null;index:idx_fs_events_timestamp"`
+	Action            string `gorm:"size:60;not null;index:idx_fs_events_action"`
+	Username          string `gorm:"size:255;not null;index:idx_fs_events_username"`
+	FsPath            string `gorm:"size:512"`
+	FsTargetPath      string `gorm:"size:512"`
+	VirtualPath       string `gorm:"size:512"`
+	VirtualTargetPath string `gorm:"size:512"`
+	SSHCmd            string `gorm:"size:60;index:idx_fs_events_ssh_cmd"`
+	FileSize          int64  `gorm:"size:64"`
+	Status            int    `gorm:"size:32;index:idx_fs_events_status"`
+	Protocol          string `gorm:"size:30;not null;index:idx_fs_events_protocol"`
+	IP                string `gorm:"size:50;index:idx_ip"`
+	InstanceID        string `gorm:"size:60;index:idx_fs_events_instance_id"`
 }
 
 func (ev *fsEventV1) TableName() string {
